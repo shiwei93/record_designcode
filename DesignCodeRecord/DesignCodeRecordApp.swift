349d5457cc0bef085deb0cafbb809c0973390b15
shiwei93 <stayfocusjs@gmail.com>
2020-10-13 18:02:36 +0800

    section 2: visual editor

---
+++ DesignCodeRecord/DesignCodeRecordApp.swift
@@ -0,0 +1,17 @@
+//
+//  DesignCodeRecordApp.swift
+//  DesignCodeRecord
+//
+//  Created by shiwei on 2020/10/13.
+//
+
+import SwiftUI
+
+@main
+struct DesignCodeRecordApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
